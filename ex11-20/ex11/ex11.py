20cdc980f71c69e3ca2643ace264e18a4e98bd13
Vincent Toon <vincenttoon22@gmail.com>
2023-09-27 21:52:05 -0500

    ex11 on main oops

---
+++ ex11-20/ex11/ex11.py
@@ -0,0 +1,21 @@
+# Asking Questions
+# Input
+
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} years old, {height} tall and {weight} heavy.")
+
+# input() is a built-in Python function that displays a prompt (specified as a string argument) to the user in the terminal
+# or console and waits for the user to enter text.
+
+# The user enters text at the prompt and presses the "Enter" key.
+
+# The input() function then captures the text entered by the user as a string and returns it as the result.
+# You can assign this result to a variable, as shown in the example with user_input.
+
+# For example, if the user enters "Hello, World!" when prompted, the value of user_input will be the string "Hello, World!".
